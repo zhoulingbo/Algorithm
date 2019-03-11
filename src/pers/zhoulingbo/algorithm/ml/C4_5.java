1ebe52633f7571dc46e947d33ac5a2bd9cd0d71d
zlb <253066156@qq.com>
2019-03-11 11:37:18 +0800

    增加一元线性回归

--- src/pers/zhoulingbo/algorithm/ml/C4_5.java
+++ src/pers/zhoulingbo/algorithm/ml/C4_5.java
@@ -1,7 +1,11 @@
 package pers.zhoulingbo.algorithm.ml;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class C4_5 extends ID3
 {
@@ -47,5 +51,83 @@ public class C4_5 extends ID3
         }
 
         return calcEntropy(map);
+    }
+    
+    private int maxGainRatioIndex(String[][] data)
+    {
+        double max = 0;
+        int index = 0;
+        for (int i = 0; i < data[0].length - 1; i++)
+        {
+            double a = gainRatio(data, i);
+            if (max < a)
+            {
+                max = a;
+                index = i;
+            }
+        }
+        return index;
+    }
+    
+    protected Node buildDecisionTree(String[][] data)
+    {
+        int index = maxGainRatioIndex(data);
+        Node node = new Node(labels[index]);
+
+        Set<String> set = new HashSet<>();
+        for (int j = 0; j < data.length; j++)
+        {
+            String[] item = data[j];
+            String key = item[index];
+            set.add(key);
+        }
+
+        for (String label : set)
+        {
+            node.labelList.add(label);
+
+            List<String[]> subList = new ArrayList<>(); // 根据标签分组数据
+            for (int i = 0; i < data.length; i++)
+            {
+                String[] item = data[i];
+                if (label.equals(item[index]))
+                    subList.add(item);
+            }
+
+            String[][] subData = new String[subList.size()][data[0].length];
+            for (int i = 0; i < subList.size(); i++)
+                subData[i] = subList.get(i);
+
+            boolean isLeaf = true;
+            String policy = null;
+            for (int k = 0; k < subList.size(); k++)
+            {
+                if (policy == null)
+                {
+                    policy = subList.get(k)[data[0].length - 1];
+                    continue;
+                }
+
+                if (!policy.equals(subList.get(k)[data[0].length - 1]))
+                {// 存在不相同的结果值，则非叶节点
+                    isLeaf = false;
+                    break;
+                }
+            }
+
+            if (isLeaf)
+            {
+                Node leaf = new Node(policy);
+                node.childList.add(leaf);
+            }
+            else
+            {
+                Node child = buildDecisionTree(subData);
+                node.childList.add(child);
+            }
+        }
+
+        return node;
+    
     }
 }
---
+++ src/pers/zhoulingbo/algorithm/ml/UnitaryLinearRegression.java
@@ -0,0 +1,91 @@
+package pers.zhoulingbo.algorithm.ml;
+
+/**
+ * 
+ * 一元线性回归 模型：y = w0+w1*x;
+ * 
+ */
+public class UnitaryLinearRegression
+{
+
+    private double[] xs = new double[] { 58, 59, 60, 61, 62 };
+    private double[] ys = new double[] { 115, 117, 120, 123, 126 };
+    private double learn_rate = 0.00003; // 学习率
+    private double w0 = 0.0;
+    private double w1 = 0.0;
+    private int step = 100; // 步数
+
+    public static void main(String[] args)
+    {
+        UnitaryLinearRegression lr = new UnitaryLinearRegression();
+        lr.gradientDescent();
+
+        System.out.println(lr.getW0() + " " + lr.getW1());
+    }
+
+    public UnitaryLinearRegression()
+    {
+
+    }
+
+    /**
+     * 梯度下降算法
+     */
+    public void gradientDescent()
+    {
+        double t0 = w0;
+        double t1 = w1;
+        for (int i = 0; i < step; i++)
+        {
+            w0 = t0 - learn_rate * partialDerivative(xs, ys, 0, t0, t1);
+            w1 = t1 - learn_rate * partialDerivative(xs, ys, 1, t0, t1);
+
+            t0 = w0;
+            t1 = w1;
+
+            System.out.println("step:" + i + "  loss:" + meanSquareError(xs, ys, t0, t1));
+        }
+    }
+
+    /**
+     * 求偏导
+     * @return
+     */
+    public double partialDerivative(double[] x_s, double[] y_s, double index, double a1, double a2)
+    {
+        double a = 0.0;
+        for (int i = 0; i < x_s.length; i++)
+        {
+            if (index > 0)
+                a += (a1 + a2 * x_s[i] - y_s[i]) * x_s[i];
+            else
+                a += a1 + a2 * x_s[i] - y_s[i];
+        }
+        return a;
+    }
+
+    public double meanSquareError(double[] x_s, double[] y_s, double a1, double a2)
+    {
+        double a = 0.0;
+        for (int i = 0; i < x_s.length; i++)
+        {
+            double s = a1 + a2 * x_s[i] - y_s[i];
+            s *= s;
+            a += s;
+        }
+
+        a = a / (2 * x_s.length);
+        return a;
+    }
+
+    public double getW0()
+    {
+        return w0;
+    }
+
+    public double getW1()
+    {
+        return w1;
+    }
+
+}
